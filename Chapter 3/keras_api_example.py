7de889fdafd2a5851e11ffa173cc16847feb8174
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-09-14 21:04:28 -0400

    Add Keras API example.

--- Chapter 3/keras_api_example.py
+++ Chapter 3/keras_api_example.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 
+# A Dense layer implemented as a Keras Layer subclass.
 class SimpleDense(keras.layers.Layer):
     def __init__(self, units, activation=None, **kwargs):
         super().__init__()
@@ -29,3 +30,15 @@ my_dense = SimpleDense(units=32, activation=tf.nn.relu)
 input_tensor = tf.ones(shape=(2, 784))
 output_tensor = my_dense(input_tensor)
 print(output_tensor.shape)
+
+# model = keras.Sequential([
+#     SimpleDense(32, 'relu'),
+#     SimpleDense(64, 'relu'),
+#     SimpleDense(32, 'relu'),
+#     SimpleDense(10, 'softmax')
+# ])
+
+model = keras.Sequential([keras.layers.Dense(1)])
+model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=1e-4),
+              loss=keras.losses.MeanSquaredError(),
+              metrics=[keras.metrics.BinaryAccuracy()])
