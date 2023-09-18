b647bb34d3d6289f1c437795a76928fe0138ccef
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-09-18 19:48:51 -0400

    Add Regression example.

--- Chapter 4/regression.py
+++ Chapter 4/regression.py
@@ -1,3 +1,8 @@
+# Another common type of machine learning problem is regression, which consists of predicting a continuous value
+# instead of a discrete label: for instance, predicting the temperature tomorrow, given meteorological data or
+# predicting the time that a software project will take to complete, given its specifications. In this example,
+# we’ll attempt to predict the median price of homes in a given Boston suburb in the mid-1970s, given data points
+# about the suburb at the time, such as the crime rate, the local property tax rate, and so on.
 def main():
     print('hi')
 
