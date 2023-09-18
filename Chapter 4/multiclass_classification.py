f47d09e49cfade56862af042359d1eb54b9e02e7
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-09-18 18:38:43 -0400

    Add Multiclass Classification example.

--- Chapter 4/multiclass_classification.py
+++ Chapter 4/multiclass_classification.py
@@ -5,8 +5,7 @@ def decode_review(review):
     word_index = reuters.get_word_index()
     reverse_word_index = dict(
         [(value, key) for (key, value) in word_index.items()])
-    print(reverse_word_index)
-    return ''.join([reverse_word_index.get(i - 3, '?') for i in review])
+    return ' '.join([reverse_word_index.get(i - 3, '?') for i in review])
 def main():
     (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 
