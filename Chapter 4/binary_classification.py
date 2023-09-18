1ca6ccb2ab5e88b10a23b3d09ad96fd9b96061d0
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-09-18 18:38:38 -0400

    Minor code refactoring.

--- Chapter 4/binary_classification.py
+++ Chapter 4/binary_classification.py
@@ -10,7 +10,7 @@ def decode_review(review):
     word_index = imdb.get_word_index()
     reverse_word_index = dict(
         [(value, key) for (key, value) in word_index.items()])
-    return ''.join(
+    return ' '.join(
         [reverse_word_index.get(i - 3, '?') for i in review])
 
 
@@ -53,6 +53,8 @@ def plot_training_and_validation_accuracy(history_dict):
 
 def main():
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+    print(decode_review(train_data[0]))
 
     # Vectorize data.
     x_train = vectorize_sequences(train_data)
